e2551222de4acaf61662d8787ef67b8942d23c91
Prateeks91 <Prateeks910@gmail.com>
2019-01-03 16:27:14 +0530

    Add files via upload

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,16 @@ import Auth from './reducer_auth';
 import TabSelect from './delivery/reducer_tabSelect';
 import getProducts from './delivery/reducer_getProducts';
 import getProfile from './delivery/reducer_getProfile';
+import getSelectedAddress from './delivery/reducer_handleSelectAddress';
+import addNewAddress from './delivery/reducer_addAddress';
 const rootReducer = combineReducers({
     commentList: CommentList,
     auth:Auth,
     tabSelected:TabSelect,
     getProducts: getProducts,
-    getProfile: getProfile
+    getProfile: getProfile,
+    getSelectedAddress: getSelectedAddress,
+    addNewAddress:addNewAddress
 });
 
 export default rootReducer;
