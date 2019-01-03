0fbb7b7a44e451f3fec8785c96300363dea350d1
Prateeks91 <Prateeks910@gmail.com>
2019-01-03 16:29:07 +0530

    Add files via upload

--- src/constants.js
+++ src/constants.js
@@ -14,7 +14,13 @@ const appConstants = {
     REVIEWPAY_ROUTE: '/checkout/reviewAndPay',
     DELIVERY_FLAG: 0,
     DATETIME_FLAG: 1,
-    REVIEWPAY_FLAG: 2
+    REVIEWPAY_FLAG: 2,
+    HANLDE_SELECT_ADDRESS:'handle select address',
+    HANDLE_ADD_ADDRESS:'handle add address',
+    PAYMENT_PROCEED: 'payment proceed',
+    SERVICE_FEE: 49,
+    VAT: 0.1,
+    SELECTED_ADDRESS:'selected address'
 }
 
 export default appConstants;
